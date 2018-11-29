16dda8e12f6ab18d91da26df529e19a8b2c686f4
罗坤 <1498710037@qq.com>
2018-11-29 22:18:40 +0800

    import ReadableMap

--- android/src/main/java/com/zhijia/WpsModule.java
+++ android/src/main/java/com/zhijia/WpsModule.java
@@ -14,6 +14,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 
 import java.io.File;
 import java.util.List;
